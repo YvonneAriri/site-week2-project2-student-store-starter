c85a9414a216d8b367ef62e76437ed4ace7a4bb4
Yvonne Ariri <yvonneariri@meta.com>
2023-06-22 23:30:13 -0700

    Styled the searchbar, HelpIcon, and MyCart button

--- student-store-ui/src/components/Home/Home.jsx
+++ student-store-ui/src/components/Home/Home.jsx
@@ -10,10 +10,17 @@ export default function Home() {
       <Hero />
       <div className="container">
         <input type="text" className="search" placeholder="Search" />
-        <button className="search-icon">
-          <i className="material-icons">search</i>
-        </button>
-        <br />
+        <i className="material-icons search-icon">search</i>
+
+        <div className="help">
+          <i style={{ color: "gray" }} className="material-icons">
+            help
+          </i>
+          <p>Help</p>
+        </div>
+        <div className="cart">
+          My Cart <i class="material-icons">shopping_cart</i>
+        </div>
       </div>
     </div>
   );
