fdc4dadd62d1063c8e1ffd688842abf7945d3437
Yvonne Ariri <yvonneariri@meta.com>
2023-06-23 08:24:38 -0700

    Implemented the categories button, so once clicked it filters the product by its category

--- student-store-ui/src/components/ProductCard/ProductCard.jsx
+++ student-store-ui/src/components/ProductCard/ProductCard.jsx
@@ -1,4 +1,6 @@
 import * as React from "react";
 import "./ProductCard.css";
-import { Link } from "react-router-dom";
-export default function ProductGrid(props) {}
+
+export default function ProductGrid(props) {
+  return <div></div>;
+}
