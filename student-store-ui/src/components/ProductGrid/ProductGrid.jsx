d0e5a3aba835caf59bb35205f2ab332f19c4e1d6
Yvonne Ariri <yvonneariri@meta.com>
2023-06-23 08:26:49 -0700

    Implemented the categories button, so once clicked it filters the product by its category(jsx file)

--- student-store-ui/src/components/ProductGrid/ProductGrid.jsx
+++ student-store-ui/src/components/ProductGrid/ProductGrid.jsx
@@ -6,14 +6,18 @@ export default function ProductGrid(props) {
   const products = props.products;
 
   const [filteredProducts, setFilteredProducts] = useState(products);
+  const [activeCategory, setActiveCategory] = useState("");
+  const [showCategories, setShowCategories] = useState(true);
 
   const filterByCategory = (category) => {
     if (!category) {
       setFilteredProducts(products);
+      setActiveCategory("all");
     } else {
       setFilteredProducts(
         products.filter((product) => product.category === category)
       );
+      setActiveCategory(category);
     }
   };
 
@@ -23,17 +27,61 @@ export default function ProductGrid(props) {
 
   return (
     <>
-      <div>
-        <button className="buttons" onClick={() => filterByCategory()}>
-          All Categories
-        </button>
-        <button onClick={() => filterByCategory("clothing")}>Clothing</button>
-        <button onClick={() => filterByCategory("food")}>Food</button>
-        <button onClick={() => filterByCategory("accessories")}>
-          Accessories
-        </button>
-        <button onClick={() => filterByCategory("tech")}>Tech</button>
+      <div className="btn">
+        <i
+          className="material-icons"
+          onClick={() => {
+            setShowCategories(!showCategories);
+          }}
+        >
+          menu
+        </i>
+        {showCategories ? (
+          <>
+            <button
+              className={`category-btn ${
+                activeCategory === "all" ? "active-btn" : ""
+              }`}
+              onClick={() => filterByCategory()}
+            >
+              All Categories
+            </button>
+            <button
+              className={`category-btn ${
+                activeCategory === "clothing" ? "active-btn" : ""
+              }`}
+              onClick={() => filterByCategory("clothing")}
+            >
+              Clothing
+            </button>
+            <button
+              className={`category-btn ${
+                activeCategory === "food" ? "active-btn" : ""
+              }`}
+              onClick={() => filterByCategory("food")}
+            >
+              Food
+            </button>
+            <button
+              className={`category-btn ${
+                activeCategory === "accessories" ? "active-btn" : ""
+              }`}
+              onClick={() => filterByCategory("accessories")}
+            >
+              Accessories
+            </button>
+            <button
+              className={`category-btn ${
+                activeCategory === "tech" ? "active-btn" : ""
+              }`}
+              onClick={() => filterByCategory("tech")}
+            >
+              Tech
+            </button>
+          </>
+        ) : null}
       </div>
+
       <div className="product-grid">
         {filteredProducts.map((product) => (
           <div key={product.id}>
