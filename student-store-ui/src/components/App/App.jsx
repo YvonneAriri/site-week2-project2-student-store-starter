b25b48a507fc83156b92befec8306641c034bb22
Yvonne Ariri <yvonneariri@meta.com>
2023-06-23 18:45:33 -0700

    Add routes and states to App component

--- student-store-ui/src/components/App/App.jsx
+++ student-store-ui/src/components/App/App.jsx
@@ -4,12 +4,48 @@ import { BrowserRouter } from "react-router-dom";
 import Navbar from "../Navbar/Navbar";
 import Sidebar from "../Sidebar/Sidebar";
 import Home from "../Home/Home";
-import ProductGrid from "../ProductGrid/ProductGrid";
-import Footer from "../Footer/Footer";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import ProductDetail from "../ProductDetail/ProductDetail";
+import NotFound from "../NotFound/NotFound";
 
 export default function App() {
   const [products, setProducts] = useState([]);
+  const [error, setError] = useState("");
+  const [isFetching, setIsFetching] = useState(true);
+  const [isOpen, setIsOpen] = useState(false);
+  const [shoppingCart, setShoppingCart] = useState([]);
+  const [checkoutForm, setCheckoutForm] = useState({});
+
+  const handleOnToggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleAddItemToCart = (productId) => {
+    const isProductInCart = false;
+    const newShoppingCart = shoppingCart.map((item) => {
+      if (item.itemId === productId) {
+        isProductInCart = true;
+        item.quantity += 1;
+      }
+    });
+
+    if (isProductInCart) {
+      setShoppingCart(newShoppingCart);
+    } else {
+      setShoppingCart([...newShoppingCart, { itemId: productId, quantity: 1 }]);
+    }
+  };
+
+  const handleRemoveItemFromCart = (productId) => {
+    const newShoppingCart = shoppingCart.map((item) => {
+      if (item.itemId === productId) {
+        item.quantity -= 1;
+      }
+    });
+
+    setShoppingCart(newShoppingCart.filter((item) => item.quantity !== 0));
+  };
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -19,8 +55,11 @@ export default function App() {
         );
         const data = response.data;
         setProducts(data.products);
+        setIsFetching(false);
       } catch (error) {
-        console.error("Error fetching products:", error);
+        const errorMessage = `Error fetching products: ${error}`;
+        setError(errorMessage);
+        console.error(errorMessage);
       }
     };
 
@@ -32,9 +71,12 @@ export default function App() {
       <BrowserRouter>
         <main>
           <Navbar />
-          <Sidebar />
-          <Home />
-          <ProductGrid products={products} />
+          <Sidebar isOpen={isOpen} handleOnToggle={handleOnToggle} />
+          <Routes>
+            <Route path="/" element={<Home products={products} />} />
+            <Route path="/products/:productId" element={<ProductDetail />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
         </main>
       </BrowserRouter>
     </div>
