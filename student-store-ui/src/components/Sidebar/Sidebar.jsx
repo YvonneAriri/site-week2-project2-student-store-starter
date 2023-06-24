d4c8c490bc4e71ff28c5ccf2681cf898db0b4fc9
Yvonne Ariri <yvonneariri@meta.com>
2023-06-23 19:00:46 -0700

    Open sidebar when arrow is clicked

--- student-store-ui/src/components/Sidebar/Sidebar.jsx
+++ student-store-ui/src/components/Sidebar/Sidebar.jsx
@@ -3,51 +3,55 @@ import ShoppingCart from "../ShoppingCart/ShoppingCart";
 import CheckoutForm from "../CheckoutForm/CheckoutForm";
 import React, { useState } from "react";
 
-export default function Sidebar() {
-  const [showAdditionalElements, setShowAdditionalElements] = useState(false);
-
-  const handleClick = () => {
-    console.log("Button clicked!");
-    setShowAdditionalElements(!showAdditionalElements);
-  };
+export default function Sidebar(props) {
+  const {
+    isOpen,
+    products,
+    shoppingCart,
+    handleOnToggle,
+    checkoutForm,
+    handleOnSubmitCheckoutForm,
+    handleOnCheckoutFormChange,
+  } = props;
 
   return (
     <section className="sidebar">
-      <ShoppingCart />
-      <CheckoutForm />
-      {!showAdditionalElements && (
-        <button className="arrowbtn" onClick={handleClick}>
-          <i style={{ color: "white" }} className="material-icons md-48">
-            arrow_forward
-          </i>
-        </button>
+      {!isOpen && (
+        <div className="section-close">
+          <button className="toggle-button" onClick={handleOnToggle}>
+            <i style={{ color: "white" }} className="material-icons md-48">
+              arrow_forward
+            </i>
+          </button>
+          <button className="cartbtn">
+            <i style={{ color: "white" }} className="material-icons md-48">
+              add_shopping_cart
+            </i>
+          </button>
+          <button className="monetizationbtn">
+            <i style={{ color: "white" }} className="material-icons md-48">
+              monetization_on
+            </i>
+          </button>
+          <button className="factbtn">
+            <i style={{ color: "white" }} className="material-icons md-48">
+              fact_check
+            </i>
+          </button>
+        </div>
       )}
-      {showAdditionalElements && (
-        <div className="additional">
-          <button className="arrowbtn" onClick={handleClick}>
+      {isOpen && (
+        <div className="section-open">
+          <button className="toggle-button" onClick={handleOnToggle}>
             <i style={{ color: "white" }} className="material-icons md-48">
               arrow_back
             </i>
           </button>
 
-          <p>ShoppingCart</p>
+          <ShoppingCart />
+          <CheckoutForm />
         </div>
       )}
-      <button className="cartbtn">
-        <i style={{ color: "white" }} className="material-icons md-48">
-          add_shopping_cart
-        </i>
-      </button>
-      <button className="monetizationbtn">
-        <i style={{ color: "white" }} className="material-icons md-48">
-          monetization_on
-        </i>
-      </button>
-      <button className="factbtn">
-        <i style={{ color: "white" }} className="material-icons md-48">
-          fact_check
-        </i>
-      </button>
     </section>
   );
 }
