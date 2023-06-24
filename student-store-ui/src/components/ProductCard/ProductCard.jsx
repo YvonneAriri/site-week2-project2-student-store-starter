f6ef04d44d6ea3ea08fffbc9781cd047b4c86006
Yvonne Ariri <yvonneariri@meta.com>
2023-06-23 22:41:28 -0700

    Styled the productCard and the add and remove button

--- student-store-ui/src/components/ProductCard/ProductCard.jsx
+++ student-store-ui/src/components/ProductCard/ProductCard.jsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import "./ProductCard.css";
 import { Link } from "react-router-dom";
 
-export default function ProductGrid(props) {
+export default function ProductCard(props) {
   const {
     product,
     productId,
@@ -13,18 +13,26 @@ export default function ProductGrid(props) {
   } = props;
 
   return (
-    <div className="layout" key={productId}>
+    <div className="product-card" key={productId}>
       <div className="media">
         <Link to={`/products/${productId}`}>
-          <img className="Image" src={product.image} alt={product.name} />
+          <img className="image" src={product.image} alt={product.name} />
         </Link>
       </div>
+      <div className="details-section">
+        <div className="name">
+          <p>{product.name}</p>
+          <p>${product.price}</p>
+        </div>
 
-      <p>{product.name}</p>
-      <p>${product.price}</p>
-      <div className="addbtn">
-        <button onClick={() => handleAddItemToCart(productId)}>+</button>
-        <button onClick={() => handleRemoveItemFromCart(productId)}>-</button>
+        <div className="buttons">
+          <button onClick={() => handleAddItemToCart(productId)}>
+            <i className="material-icons">add</i>
+          </button>
+          <button onClick={() => handleRemoveItemFromCart(productId)}>
+            <i className="material-icons">remove</i>
+          </button>
+        </div>
       </div>
     </div>
   );
