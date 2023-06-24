2eeae0a1fcbf7ed20cef33aee6fab3e26584f362
Yvonne Ariri <yvonneariri@meta.com>
2023-06-23 22:45:43 -0700

    Implemented the links in the Navbar

--- student-store-ui/src/components/Navbar/Navbar.jsx
+++ student-store-ui/src/components/Navbar/Navbar.jsx
@@ -5,6 +5,7 @@ import Logo from "../Logo/Logo";
 import twitterIcon from "./twitter.svg";
 import instagramIcon from "./instagram.svg";
 import facebookIcon from "./facebook.svg";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
@@ -13,11 +14,14 @@ export default function Navbar() {
       <img className="twittericon" src={twitterIcon} />
       <img className="instagramicon" src={instagramIcon} />
       <img className="facebookicon" src={facebookIcon} />
-
-      <p className="homebar">Home</p>
+      <a href="#homebar">Home</a>
+      <a href="#aboutbar">About Us</a>
+      <a href="#contact-us">Contact Us</a>
+      <a href="#buybar">Buy Now</a>
+      {/* <p className="homebar">Home</p>
       <p className="aboutbar">About Us</p>
       <p className="contactbar">Contact Us</p>
-      <p className="buybar">Buy Now</p>
+      <p className="buybar">Buy Now</p> */}
     </nav>
   );
 }
