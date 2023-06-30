d61bd29d988e250003b134d13755f3352d77817a
Yvonne Ariri <yvonneariri@meta.com>
2023-06-29 20:37:36 -0700

    made use of the parsefloat function to show only two decimal places. Also implemented the Tax and Fees and Total.

--- student-store-ui/src/components/ShoppingCart/ShoppingCart.jsx
+++ student-store-ui/src/components/ShoppingCart/ShoppingCart.jsx
@@ -7,15 +7,12 @@ export default function ShoppingCart(props) {
   const { isOpen, products, shoppingCart } = props;
 
   const getItemName = (itemId) => {
-    // Find the product with the matching itemId in the products array
     const product = products.find((item) => item.id === itemId);
-    // Return the product's name if found, or an empty string otherwise
     return product ? product.name : "";
   };
   const getItemPrice = (itemId) => {
-    // Find the product with the matching itemId in the products array
     const product = products.find((item) => item.id === itemId);
-    // Return the product's price if found, or 0 otherwise
+
     return product ? product.price : 0;
   };
   const getItemCost = (itemId, quantity) => {
@@ -28,42 +25,55 @@ export default function ShoppingCart(props) {
   }, 0);
   const subtotal = shoppingCart.reduce((total, item) => {
     const price = getItemPrice(item.itemId);
-    return total + price;
+    return parseFloat((total + price * item.quantity).toFixed(2));
   }, 0);
+  const taxRate = 0.0875;
+  const tax = parseFloat((subtotal * taxRate).toFixed(2));
+  const Total = parseFloat((subtotal + tax).toFixed(2));
 
   return (
-    <div className="cart-product-name">
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Quantity</th>
-            <th>Unit price</th>
-            <th>Cost</th>
-          </tr>
-          <tr></tr>
-        </thead>
-        <tbody>
-          {shoppingCart.map((item) => (
-            <tr key={item.itemId}>
-              <td>{getItemName(item.itemId)}</td>
-              <td>{item.quantity}</td>
-              <td>{getItemPrice(item.itemId)}</td>
-              <td>{getItemCost(item.itemId, item.quantity)}</td>
+    <>
+      <div className="cart-product-name">
+        <table className="items">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Quantity</th>
+              <th>Unit price</th>
+              <th>Cost</th>
             </tr>
-          ))}
-        </tbody>
-        <tfoot>
-          <tr>
-            <td colSpan="3">Subtotal:</td>
-            <td>{subtotal}</td>
-          </tr>
-          {/* <tr>
-            <td colSpan="3">Total Cost:</td>
-            <td>{totalCost}</td>
-          </tr> */}
-        </tfoot>
-      </table>
-    </div>
+            <tr></tr>
+          </thead>
+          <tbody>
+            {shoppingCart.map((item) => (
+              <tr key={item.itemId}>
+                <td>{getItemName(item.itemId)}</td>
+                <td>{item.quantity}</td>
+                <td>{getItemPrice(item.itemId)}</td>
+                <td>{getItemCost(item.itemId, item.quantity)}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <div>
+        <table className="total">
+          <tfoot>
+            <tr>
+              <td colSpan="3">Subtotal:</td>
+              <td>{subtotal}</td>
+            </tr>
+            <tr>
+              <td colSpan="3">Tax and Fees:</td>
+              <td>{tax}</td>
+            </tr>
+            <tr>
+              <td colSpan="3">Total:</td>
+              <td>{Total}</td>
+            </tr>
+          </tfoot>
+        </table>
+      </div>
+    </>
   );
 }
